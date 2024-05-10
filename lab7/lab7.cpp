8ea1c2346db4bbb33db77864d76e54717e6163f1
gmh1627 <3558965271@qq.com>
2024-05-10 15:46:58 +0800

    commit

--- lab7/lab7.cpp
+++ lab7/lab7.cpp
@@ -2,84 +2,45 @@
 using namespace std;
 
 int satisfiedCount;
+int maxIter;
+long double eps = 1e-6, proportion;
+vector<long double> Vx(100), Vy(100);
 
+void Calculate(int maxIter);
 long double ax(long double t);
 long double ay(long double t);
-long double romberg(long double (*f)(long double), long double a, long double b, long double eps, int maxIter, bool isX);// Perform the Romberg integration
+long double vx(long double t);
+long double vy(long double t);
+long double romberg(function<long double(long double)> f, long double a, long double b, long double eps, int maxIter, bool isX);// Perform the Romberg integration
 
 int main() 
 {
-    long double eps = 1e-6, proportion;
-    int maxIter;
-
-    satisfiedCount = 0;
-    maxIter = 4;
-    cout << "maxIter = " << maxIter << endl;
-    for (long double t = 0.1; t <= 10; t += 0.1) 
-    { 
-        long double x = romberg(ax, 0, t, eps, maxIter, 1);
-        long double y = romberg(ay, 0, t, eps, maxIter, 0);
-        cout << fixed << setprecision(1) << "At t = " << t << ", (x, y) = (" << setprecision(6) << x << ", " << setprecision(6) << y << ")" << endl;
-    }
-    cout << satisfiedCount << endl;
-    proportion = (long double)satisfiedCount / 100;
-    cout << "At maxIter = " << maxIter << ", proportion of times the error requirement of x was satisfied: " << proportion << endl;
-
-    satisfiedCount = 0;
-    maxIter = 8;
-    cout << "maxIter = " << maxIter << endl;
-    ofstream outFile("trajectory.txt");
-    for (long double t = 0.1; t <= 10; t += 0.1) 
-    {
-        long double x = romberg(ax, 0, t, eps, maxIter, 1);
-        long double y = romberg(ay, 0, t, eps, maxIter, 0);
-        cout << fixed << setprecision(1) << "At t = " << t << ", (x, y) = (" << setprecision(6) << x << ", " << setprecision(6) << y << ")" << endl;
-        outFile << fixed << setprecision(6) << x << " " << y << "\n";
-    }
-    cout << satisfiedCount << endl;
-    proportion = (long double)satisfiedCount / 100;
-    cout << "At maxIter = " << maxIter << ", proportion of times the error requirement of x was satisfied: " << proportion << endl;
-
-    satisfiedCount = 0;
-    maxIter = 12;
-    cout << "maxIter = " << maxIter << endl;
-    for (long double t = 0.1; t <= 10; t += 0.1) 
-    {
-        long double x = romberg(ax, 0, t, eps, maxIter, 1);
-        long double y = romberg(ay, 0, t, eps, maxIter, 0);
-        cout << fixed << setprecision(1) << "At t = " << t << ", (x, y) = (" << setprecision(6) << x << ", " << setprecision(6) << y << ")" << endl;
-    }
-    cout << satisfiedCount << endl;
-    proportion = (long double)satisfiedCount / 100;
-    cout << "At maxIter = " << maxIter << ", proportion of times the error requirement of x was satisfied: " << proportion << endl;
-
-    satisfiedCount = 0;
-    maxIter = 16;
-    cout << "maxIter = " << maxIter << endl;
-    for (long double t = 0.1; t <= 10; t += 0.1) 
-    {
-        long double x = romberg(ax, 0, t, eps, maxIter, 1);
-        long double y = romberg(ay, 0, t, eps, maxIter, 0);
-        cout << fixed << setprecision(1) << "At t = " << t << ", (x, y) = (" << setprecision(6) << x << ", " << setprecision(6) << y << ")" << endl;
-    }
-    cout << satisfiedCount << endl;
-    proportion = (long double)satisfiedCount / 100;
-    cout << "At maxIter = " << maxIter << ", proportion of times the error requirement of x was satisfied: " << proportion << endl;
-
-    satisfiedCount = 0;
-    maxIter = 20;
-    cout << "maxIter = " << maxIter << endl;
-    for (long double t = 0.1; t < 10.1; t += 0.1) 
-    {
-        long double x = romberg(ax, 0, t, eps, maxIter, 1);
-        long double y = romberg(ay, 0, t, eps, maxIter, 0);
-        cout << fixed << setprecision(1) << "At t = " << t << ", (x, y) = (" << setprecision(6) << x << ", " << setprecision(6) << y << ")" << endl;
-    }
-    cout << satisfiedCount << endl;
-    proportion = (long double)satisfiedCount / 100;
-    cout << "At maxIter = " << maxIter << ", proportion of times the error requirement of x was satisfied: " << proportion << endl;
+    Calculate(4);
+    Calculate(8);
+    Calculate(12);
+    Calculate(16);
+    Calculate(20);
 
     return 0;
+}
+
+void Calculate(int maxIter) {
+    satisfiedCount = 0; // use the global variable
+    cout << "maxIter = " << maxIter << endl;
+    ofstream outFile("trajectory.txt");
+    for (long double t = 0.1; t < 10.1; t += 0.1) 
+    { 
+        Vx[10*t-1] = romberg(ax, 0, t, eps, maxIter, 0);
+        Vy[10*t-1] = romberg(ay, 0, t, eps, maxIter, 0);
+        long double x = romberg(vx, 0, t, eps, maxIter, 1);
+        long double y = romberg(vy, 0, t, eps, maxIter, 0);
+        cout << fixed << setprecision(1) << "At t = " << t << ", vx = " << setprecision(6) << Vx[10*t-1] << ", vy = " << setprecision(6) << Vy[10*t-1] << ", " << "(x, y) = (" << setprecision(6) << x << ", " << setprecision(6) << y << ")" << endl;
+
+        if (maxIter == 8)
+            outFile << fixed << setprecision(6) << x << " " << y << "\n";
+    }
+    long double proportion = (long double)satisfiedCount / 100;
+    cout << "At maxIter = " << maxIter << ", proportion of times the error requirement of x was satisfied: " << proportion << endl;
 }
 
 long double ax(long double t) 
@@ -92,8 +53,18 @@ long double ay(long double t)
     return log(t + 1) / (t + 1);
 }
 
+long double vx(long double t) 
+{
+    return Vx[10*t-1];
+}
+
+long double vy(long double t) 
+{
+    return Vy[10*t-1];
+}
+
 // Perform the Romberg integration
-long double romberg(long double (*f)(long double), long double a, long double b, long double eps, int maxIter, bool isX) {
+long double romberg(function<long double(long double)> f, long double a, long double b, long double eps, int maxIter, bool isX) {
     long double h[maxIter], r[maxIter][maxIter];
     h[0] = b - a;
     r[0][0] = 0.5 * h[0] * (f(a) + f(b));
--- lab7/visualization.py
+++ lab7/visualization.py
@@ -6,7 +6,7 @@ with open('trajectory.txt', 'r') as file:
 x, y = zip(*[(float(line.split()[0]), float(line.split()[1])) for line in lines])
 
 plt.plot(x, y, 'o-')
-plt.xlabel('X')  # Change the x-axis label to 'X'
-plt.ylabel('Y')  # Change the y-axis label to 'Y'
-plt.title('Plot of points with smooth curve')
+plt.xlabel('X')
+plt.ylabel('Y')
+plt.title('Trajectory of the particle')
 plt.show()
