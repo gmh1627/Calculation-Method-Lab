e0b089c9eee5d2299c72ae757433fdf7306e6c71
gmh1627 <3558965271@qq.com>
2024-05-12 17:24:52 +0800

    commit

---
+++ lab5/HW5-PB22061161-高茂航/src/lab5.cpp
@@ -0,0 +1,169 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+//三次样条插值
+vector<long double> Spline_Interpolation(const vector<long double>& x, const vector<long double>& y);
+// 列主元消去法求解线性方程组
+vector<long double> Column_Elimination(vector<vector<long double>> A, vector<long double> b);
+
+int main()
+{
+    ifstream file("point.txt");
+    if (!file) {
+        cerr << "Unable to open file point.txt";
+        return 1;
+    }
+
+    vector<long double> x, y, lambda, d, h1, miu, h2;
+    long double a, b;
+    while (file >> a >> b)
+    {
+        x.push_back(a);
+        y.push_back(b);
+        lambda.push_back(0);
+        d.push_back(0);
+        h1.push_back(0);
+        h2.push_back(0);
+        miu.push_back(0);
+    }
+    
+    cout << "原始数据插值结果：" << endl;
+    vector<long double> M1 = Spline_Interpolation(x, y);
+    vector<vector<long double>> S1(M1.size()-1, vector<long double>(4, 0));
+    for(int i = 0; i < M1.size(); i++)
+    {
+        cout << "M1[" << i << "]:" << M1[i] << endl;
+        h1[i] = x[i + 1] - x[i];
+    }    
+    cout << endl;
+    for(int i = 0; i < M1.size() - 1; i++)
+    {
+        cout << "S1[" << i << "]:";
+        S1[i][0] = (M1[i + 1] - M1[i]) / (6 * h1[i]);
+        cout << S1[i][0] << "x^3";
+        S1[i][1] = (x[i + 1] * M1[i] - x[i] * M1[i + 1]) / (2 * h1[i]);
+        if(S1[i][1] >= 0)
+            cout << "+" ;
+        cout << S1[i][1] << "x^2";
+        S1[i][2] = (3 * M1[i+1] * x[i] * x[i] - 3 * M1[i] * x[i + 1] * x[i + 1] - 6 * y[i] + 6 * y[i+1] + h1[i] * h1[i] * M1[i] - h1[i] * h1[i] * M1[i + 1]) / (6 * h1[i]);
+        if(S1[i][2] >= 0)
+            cout << "+" ;
+        cout << S1[i][2] << "x";
+        S1[i][3] = (x[i + 1] * x[i + 1] * x[i + 1] * M1[i] - x[i] * x[i] * x[i] * M1[i + 1] + 6 * x[i+1] * y[i] - 6 * x[i] * y[i+1] - h1[i] * h1[i] * M1[i] * x[i+1] + h1[i] * h1[i] * M1[i+1] * x[i]) / (6 * h1[i]);
+        if(S1[i][3] >= 0)
+            cout << "+" ;
+        cout << S1[i][3];
+        cout << endl;
+    }
+
+    //修改第十个压铁的坐标为(0,10)
+    cout << endl << "修改第十个压铁的坐标为(0,10)后：" << endl;
+    y[9] = 10;
+    vector<long double> M2 = Spline_Interpolation(x, y);
+    vector<vector<long double>> S2(M2.size()-1, vector<long double>(4, 0));
+    for(int i = 0; i < M2.size(); i++)
+    {
+        cout << "M2[" << i << "]:" << M2[i] << endl;
+        h2[i] = x[i + 1] - x[i];
+    }
+    cout << endl;
+    for(int i = 0; i < M2.size() - 1; i++)
+    {
+        cout << "S2[" << i << "]:";
+        S2[i][0] = (M2[i + 1] - M2[i]) / (6 * h1[i]);
+        cout << S2[i][0] << "x^3";
+        S2[i][1] = (x[i + 1] * M1[i] - x[i] * M2[i + 1]) / (2 * h1[i]);
+        if(S2[i][1] >= 0)
+            cout << "+" ;
+        cout << S2[i][1] << "x^2";
+        S2[i][2] = (3 * M2[i+1] * x[i] * x[i] - 3 * M2[i] * x[i + 1] * x[i + 1] - 6 * y[i] + 6 * y[i+1] + h2[i] * h2[i] * M2[i] - h2[i] * h2[i] * M2[i + 1]) / (6 * h2[i]);
+        if(S2[i][2] >= 0)
+            cout << "+" ;
+        cout << S2[i][2] << "x";
+        S2[i][3] = (x[i + 1] * x[i + 1] * x[i + 1] * M2[i] - x[i] * x[i] * x[i] * M2[i + 1] + 6 * x[i+1] * y[i] - 6 * x[i] * y[i+1] - h2[i] * h2[i] * M2[i] * x[i+1] + h2[i] * h2[i] * M2[i+1] * x[i]) / (6 * h2[i]);
+        if(S2[i][3] >= 0)
+            cout << "+" ;
+        cout << S2[i][3];
+        cout << endl;
+    }
+    return 0;
+}
+
+//三次样条插值
+vector<long double> Spline_Interpolation(const vector<long double>& x, const vector<long double>& y) 
+{
+    int len = x.size();
+    int n = len - 1;
+    vector<long double> h(n), lambda(n), miu(n), d(n);
+    for(int i = 0; i < n; i++)
+        h[i] = x[i + 1] - x[i];
+    for(int i = 1; i < n; i++)
+    {
+        lambda[i] = h[i] / (h[i] + h[i - 1]);
+        miu[i] = 1 - lambda[i];
+        d[i] = 6 / (h[i] + h[i - 1]) * ((y[i + 1] - y[i]) / h[i] - (y[i] - y[i - 1]) / h[i - 1]);
+    }
+    vector<vector<long double>> A(n - 1, vector<long double>(n - 1, 0));
+    for(int i = 0; i < n - 1; i++)
+    {
+        A[i][i] = 2;
+        if(i != 0)
+            A[i][i - 1] = miu[i + 1];
+        if(i != n - 2)
+            A[i][i + 1] = lambda[i + 1];
+    }
+    vector<long double> B(n - 1, 0);
+    for(int i = 0; i < n - 1; i++)
+        B[i] = d[i + 1];
+    vector<long double> M = Column_Elimination(A, B);
+    return M;
+}
+
+// 列主元消去法求解线性方程组
+vector<long double> Column_Elimination(vector<vector<long double>> A, vector<long double> b)
+{
+    int n = A.size();
+    vector<long double> x(n + 2, 0);
+    vector<vector<long double>> matrix(n, vector<long double>(n + 1, 0));
+
+    for(int i = 0; i < n; i++)
+    {
+        matrix[i][n] = b[i];
+        for(int j = 0; j < n; j++)
+            matrix[i][j] = A[i][j];
+    }
+        
+    for(int col = 0; col < n; col++)//找到列主元
+    {
+        long double maxnum = abs(matrix[col][col]);
+        int maxrow = col;
+        for(int row = col + 1; row < n; row++)
+        {
+            if(abs(matrix[row][col]) > maxnum)
+            {
+                maxnum = abs(matrix[row][col]);
+                maxrow = row;
+            }
+        }
+        swap(matrix[col], matrix[maxrow]);
+        for(int row = col + 1; row < n; row++)
+        {
+            long double res = matrix[row][col] / matrix[col][col];
+            for(int loc = col; loc <= n; loc++)
+                matrix[row][loc] -= matrix[col][loc] * res; 
+        }
+    }
+    for(int row = n - 1; row >= 0; row--)//回代
+    {
+        for(int col = row + 1; col < n; col++)
+        {
+            matrix[row][n] -= matrix[col][n] * matrix[row][col] / matrix[col][col];
+            matrix[row][col] = 0;
+        }
+        matrix[row][n] /= matrix[row][row];
+        matrix[row][row] = 1;
+    }
+    for(int i = 0; i < n; i++)
+        x[i+1] = matrix[i][n];
+    return x;
+}
